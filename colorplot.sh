936c83327b8f9d382875ea31157c04212196a8f4
hchasan <hasan.112@osu.edu>
2017-05-22 20:27:15 -0400

    new script: colorplot_loop.sh
    colorplot_loop prompts user for x- and y- axis variables, then creates many plots with those two axes and with every combination of constants. New instructions have been added as needed to colorplot_instructions, plus a bonus dodo bird

--- colorplot.sh
+++ colorplot.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #04/18/2017
-#script will collect root files and plot as dictated by user
+# script will collect root files and plot as dictated by user
 #Hannah Hasan
 
-shelfmc=$HOME/ShelfMC/git_shelfmc ##set to your ShelfMC directory - should have the parameter space scan directory and a directory called "outputs". outputs may be empty
+shelfmc=$HOME/InIceMCScan ##set to your ShelfMC directory - should have the parameter space scan directory and a directory called "outputs". outputs may be empty
 
 line1='{'
-line2='  TFile *f = new TFile("'"Veff_${xvar}_${yvar}.root"'");' ##set later
+line2='  TFile *f = new TFile("'"Veff_${xvar}_${yvar}.root"'");'   #set later
 line3='  TTree *SimParam = (TTree*)f->Get("sim_parameters");'
 line4='  TCanvas *c = new TCanvas("c", "V_{eff}", 2000, 1000);'
 line5='  TPad* p = new TPad("p","p",0.0,0.0,1.0,1.0);'
@@ -14,32 +14,32 @@ line6='  p->Draw();'
 line7='  p->SetLeftMargin(0.15);'
 line8='  p->SetRightMargin(0.15);'
 line9='  p->cd();'
-line10='  TH2F *h = new TH2F("''h''", "''V_{eff} vs '"$var1"' vs '"$var2"'"'",$xbins,$xmin,$xmax"', '"$ybins,$ymin,$ymax);" ##set later
+line10='  TH2F *h = new TH2F("''h''", "''V_{eff} vs '"$var1"' vs '"$var2"'"'",$xbins,$xmin,$xmax"', '"$ybins,$ymin,$ymax);"   set later
 line11='  double x;'
 line12='  double y;'
 line13='  double z;'
 line14='  double veff=0;'
 line15='  double energy;'
-line16="  double avgs[$SIZE];" ##set later; SIZE varies depending on which variables we are plotting
-line17='  SimParam->SetBranchAddress("'"$xbranch"'", &x );' ##set later
-line18='  SimParam->SetBranchAddress("'"$ybranch"'", &y );' ##set later
+line16="  double avgs[$SIZE];"   #set later; SIZE varies depending on which variables we are plotting
+line17='  SimParam->SetBranchAddress("'"$xbranch"'", &x );'   #set later
+line18='  SimParam->SetBranchAddress("'"$ybranch"'", &y );'   #set later
 line19='  SimParam->SetBranchAddress("Veff", &z);'
 line20='  int v=0;'
-line21="  for(int k=1; k<=$SIZE; k++){" ##set later for SIZE
+line21="  for(int k=1; k<=$SIZE; k++){"   #set later for SIZE
 line22='    veff = 0;'
-line23='    for(v; v<(k*20.0); v++){'     ##change 20 to number of processors ShelfMC runs were split across
+line23='    for(v; v<(k*20.0); v++){'       #change 20 to number of processors ShelfMC runs were split across
 line24='      SimParam->GetEntry(v);'
 line25='      veff += z;'
 line26='    }'
-line27='    avgs[(k-1)] = (veff/20.0);'    ##change 20 to number of processors
+line27='    avgs[(k-1)] = (veff/20.0);'      #change 20 to number of processors
 line28='  }'
-line29="  for(int i=0; i<$SIZE; i++){" ##set later for SIZE
-line30='    SimParam->GetEntry((i*20));'     ##change 20 to number of processors
+line29="  for(int i=0; i<$SIZE; i++){"   #set later for SIZE
+line30='    SimParam->GetEntry((i*20));'       #change 20 to number of processors
 line31='    h->Fill(x,y,avgs[i]);'
 line32='  }'
 line33='  gStyle->SetPalette(1,0);'
-line34='  h->GetXaxis()->SetTitle("'"$var1"' (m)");' ##set later
-line35='  h->GetYaxis()->SetTitle("'"$var2"' (m)");' ##set later
+line34='  h->GetXaxis()->SetTitle("'"$var1"' (m)");'   #set later
+line35='  h->GetYaxis()->SetTitle("'"$var2"' (m)");'   #set later
 line36='  h->GetZaxis()->SetTitle("V_{eff} (km^{3} str)");'
 line37='  h->GetXaxis()->SetTitleOffset(1.2);'
 line38='  h->GetYaxis()->SetTitleOffset(1.0);'
@@ -49,9 +49,9 @@ line41='  h->Draw("COLZ");'
 line42='  double const1;'
 line43='  double const2;'
 line44='  double const3;'
-line45='  SimParam->SetBranchAddress("'"$const1br"'", &const1);' ##set later
-line46='  SimParam->SetBranchAddress("'"$const2br"'", &const2);' ##set later
-line47='  SimParam->SetBranchAddress("'"$const3br"'", &const3);' ##set later
+line45='  SimParam->SetBranchAddress("'"$const1br"'", &const1);'   #set later
+line46='  SimParam->SetBranchAddress("'"$const2br"'", &const2);'   #set later
+line47='  SimParam->SetBranchAddress("'"$const3br"'", &const3);'   #set later
 line48='  SimParam->SetBranchAddress("exponent", &energy);'
 line49='  SimParam->GetEntry(0);'
 line50='  char const_1[30];'
@@ -59,9 +59,9 @@ line51='  char const_2[30];'
 line52='  char const_3[30];'
 line53='  char enrgy[30];'
 line54='  sprintf(enrgy, "Energy (exponent): %d", energy);'
-line55='  sprintf(const_1, "'"$const1br"' %d m", const1);' ##
-line56='  sprintf(const_2, "'"$const2br"' %d m", const2);' ##
-line57='  sprintf(const_3, "'"$const3br"' %d m", const3);' ## set later
+line55='  sprintf(const_1, "'"$const1br"' %d m", const1);'   
+line56='  sprintf(const_2, "'"$const2br"' %d m", const2);'   
+line57='  sprintf(const_3, "'"$const3br"' %d m", const3);'    #set later
 line58='  TPaveText *t = new TPaveText(0,0,0.07,0.3,"brNDC");'
 line59='  t->SetLabel("Simulation Parameters:");'
 line60='  t->AddText(enrgy);'
@@ -71,7 +71,7 @@ line63='  t->AddText(const_3);'
 line64='  t->Draw();'
 line65='  c->Modified();'
 line66='  c->Update();'
-line67='  c->SaveAs("'"Veff_${xvar}_${yvar}.pdf"'");' ##set later
+line67='  c->SaveAs("'"Veff_${xvar}_${yvar}.pdf"'");'   #set later
 line68='}'
 
 
@@ -80,7 +80,7 @@ echo
 echo 'This script will allow you to choose two variables to plot effective volume against.'
 echo
 echo 'Enter the name of the run you wish to plot data for.'
-## echo '***IMPORTANT*** please ensure there is no existing directory of this name in the "outputs" directory!'
+   echo '***IMPORTANT*** please ensure there is no existing directory of this name in the "outputs" directory!'
 echo
 read -p 'Name of run: ' runName
 echo
@@ -121,17 +121,17 @@ stprompt='Select what value at which to hold constant Station Depth'
 stinc=' > Choose only values between 0-200, at increments of 50: '
 
 if ([ "$xvar" = "A" ] && [ "$yvar" = "R" ]) || ([ "$yvar" = "A" ] && [ "$xvar" = "R" ]); then
-    echo $iceprompt
-    echo $iceinc
-    read const1
+     echo $iceprompt
+     echo $iceinc
+     read const1
     const1br='icethick'
-    echo $firnprompt
-    echo $firninc
-    read const2
+     echo $firnprompt
+     echo $firninc
+     read const2
     const2br='firndepth'
-    echo $stprompt
-    echo $stinc
-    read const3
+     echo $stprompt
+     echo $stinc
+     read const3
     const3br='station_depth'
     echo
     echo 'Adding files...'
@@ -149,17 +149,17 @@ if ([ "$xvar" = "A" ] && [ "$yvar" = "R" ]) || ([ "$yvar" = "A" ] && [ "$xvar" =
     done
 
 elif ([ "$xvar" = "A" ] && [ "$yvar" = "I" ]) || ([ "$yvar" = "A" ] && [ "$xvar" = "I" ]); then
-    echo $radiusprompt
-    echo $radiusinc
-    read const1
+     echo $radiusprompt
+     echo $radiusinc
+     read const1
     const1br='st4_r'
-    echo $firnprompt
-    echo $firninc
-    read const2
+     echo $firnprompt
+     echo $firninc
+     read const2
     const2br='firndepth'
-    echo $stprompt
-    echo $stinc
-    read const3
+     echo $stprompt
+     echo $stinc
+     read const3
     const3br='station_depth'
     echo
     for L in {500..1000..100}
@@ -175,17 +175,17 @@ elif ([ "$xvar" = "A" ] && [ "$yvar" = "I" ]) || ([ "$yvar" = "A" ] && [ "$xvar"
     done
 
 elif ([ "$xvar" = "A" ] && [ "$yvar" = "F" ]) || ([ "$yvar" = "A" ] && [ "$xvar" = "F" ]); then
-    echo $radiusprompt
-    echo $radiusinc
-    read const1
+     echo $radiusprompt
+     echo $radiusinc
+     read const1
     const1br='st4_r'
-    echo $iceprompt
-    echo $iceinc
-    read const2
+     echo $iceprompt
+     echo $iceinc
+     read const2
     const2br='icethick'
-    echo $stprompt
-    echo $stinc
-    read const3
+     echo $stprompt
+     echo $stinc
+     read const3
     const3br='station_depth'
     echo
     for L in {500..1000..100}
@@ -201,17 +201,17 @@ elif ([ "$xvar" = "A" ] && [ "$yvar" = "F" ]) || ([ "$yvar" = "A" ] && [ "$xvar"
     done
 
 elif ([ "$xvar" = "A" ] && [ "$yvar" = "S" ]) || ([ "$yvar" = "A" ] && [ "$xvar" = "S" ]); then
-    echo $radiusprompt
-    echo $radiusinc
-    read const1
+     echo $radiusprompt
+     echo $radiusinc
+     read const1
     const1br='st4_r'
-    echo $iceprompt
-    echo $iceinc
-    read const2
+     echo $iceprompt
+     echo $iceinc
+     read const2
     const2br='icethick'
-    echo $firnprompt
-    echo $firninc
-    read const3
+     echo $firnprompt
+     echo $firninc
+     read const3
     const3br='firndepth'
     echo
     for L in {500..1000..100}
@@ -227,17 +227,17 @@ elif ([ "$xvar" = "A" ] && [ "$yvar" = "S" ]) || ([ "$yvar" = "A" ] && [ "$xvar"
     done
 
 elif ([ "$xvar" = "R" ] && [ "$yvar" = "I" ]) || ([ "$yvar" = "R" ] && [ "$xvar" = "I" ]); then
-    echo $attenprompt
-    echo $atteninc
-    read const1
+     echo $attenprompt
+     echo $atteninc
+     read const1
     const1br='atten_up'
-    echo $firnprompt
-    echo $firninc
-    read const2
+     echo $firnprompt
+     echo $firninc
+     read const2
     const2br='firndepth'
-    echo $stprompt
-    echo $stinc
-    read const3
+     echo $stprompt
+     echo $stinc
+     read const3
     const3br='station_depth'
     echo
     for AR in {3..31..7}
@@ -253,17 +253,17 @@ elif ([ "$xvar" = "R" ] && [ "$yvar" = "I" ]) || ([ "$yvar" = "R" ] && [ "$xvar"
     done
 
 elif ([ "$xvar" = "R" ] && [ "$yvar" = "F" ]) || ([ "$yvar" = "R" ] && [ "$xvar" = "F" ]); then
-    echo $attenprompt
-    echo $atteninc
-    read const1
+     echo $attenprompt
+     echo $atteninc
+     read const1
     const1br='atten_up'
-    echo $iceprompt
-    echo $iceinc
-    read const2
+     echo $iceprompt
+     echo $iceinc
+     read const2
     const2br='icethick'
-    echo $stprompt
-    echo $stinc
-    read const3
+     echo $stprompt
+     echo $stinc
+     read const3
     const3br='station_depth'
     echo
     for AR in {3..31..7}
@@ -279,17 +279,17 @@ elif ([ "$xvar" = "R" ] && [ "$yvar" = "F" ]) || ([ "$yvar" = "R" ] && [ "$xvar"
     done
 
 elif ([ "$xvar" = "R" ] && [ "$yvar" = "S" ]) || ([ "$yvar" = "R" ] && [ "$xvar" = "S" ]); then
-    echo $attenprompt
-    echo $atteninc
-    read const1
+     echo $attenprompt
+     echo $atteninc
+     read const1
     const1br='atten_up'
-    echo $iceprompt
-    echo $iceinc
-    read const2
+     echo $iceprompt
+     echo $iceinc
+     read const2
     const2br='icethick'
-    echo $firnprompt
-    echo $firninc
-    read const3
+     echo $firnprompt
+     echo $firninc
+     read const3
     const3br='firndepth'
     echo
     for AR in {3..31..7}
@@ -305,17 +305,17 @@ elif ([ "$xvar" = "R" ] && [ "$yvar" = "S" ]) || ([ "$yvar" = "R" ] && [ "$xvar"
     done
 
 elif ([ "$xvar" = "I" ] && [ "$yvar" = "F" ]) || ([ "$yvar" = "I" ] && [ "$xvar" = "F" ]); then
-    echo $attenprompt
-    echo $atteninc
-    read const1
+     echo $attenprompt
+     echo $atteninc
+     read const1
     const1br='atten_up'
-    echo $radiusprompt
-    echo $radiusinc
-    read const2
+     echo $radiusprompt
+     echo $radiusinc
+     read const2
     const2br='st4_r'
-    echo $stprompt
-    echo $stinc
-    read const3
+     echo $stprompt
+     echo $stinc
+     read const3
     const3br='station_depth'
     echo
     for Ice in {500..2900..400}
@@ -331,17 +331,17 @@ elif ([ "$xvar" = "I" ] && [ "$yvar" = "F" ]) || ([ "$yvar" = "I" ] && [ "$xvar"
     done
 
 elif ([ "$xvar" = "I" ] && [ "$yvar" = "S" ]) || ([ "$yvar" = "I" ] && [ "$xvar" = "S" ]); then
-    echo $attenprompt
-    echo $atteninc
-    read const1
+     echo $attenprompt
+     echo $atteninc
+     read const1
     const1br='atten_up'
-    echo $radiusprompt
-    echo $radiusinc
-    read const2
+     echo $radiusprompt
+     echo $radiusinc
+     read const2
     const2br='st4_r'
-    echo $firnprompt
-    echo $firninc
-    read const3
+     echo $firnprompt
+     echo $firninc
+     read const3
     const3br='firndepth'
     echo
     for Ice in {500..2900..400}
@@ -357,17 +357,17 @@ elif ([ "$xvar" = "I" ] && [ "$yvar" = "S" ]) || ([ "$yvar" = "I" ] && [ "$xvar"
     done
 
 elif ([ "$xvar" = "F" ] && [ "$yvar" = "S" ]) || ([ "$yvar" = "F" ] && [ "$xvar" = "S" ]); then
-    echo $attenprompt
-    echo $atteninc
-    read const1
+     echo $attenprompt
+     echo $atteninc
+     read const1
     const1br='atten_up'
-    echo $radiusprompt
-    echo $radiusinc
-    read const2
+     echo $radiusprompt
+     echo $radiusinc
+     read const2
     const2br='st4_r'
-    echo $iceprompt
-    echo $iceinc
-    read const3
+     echo $iceprompt
+     echo $iceinc
+     read const3
     const3br='icethick'
     echo
     for FT in {60..140..20}
@@ -453,7 +453,7 @@ fi
 
 let "SIZE=$xbins*$ybins"
 
-####### all changing lines below:
+        all changing lines below:
 line2='  TFile *f = new TFile("'"Veff_${xvar}_${yvar}.root"'");'
 line10='  TH2F *h = new TH2F("h", "''V_{eff} vs '"$var1 vs $var2"'"'",$xbins,$xmin,$xmax"', '"$ybins,$ymin,$ymax);"
 line16="  double avgs[$SIZE];"
@@ -482,7 +482,11 @@ for t in {1..68}; do
 done
 
 rm *Result*
-root -l Veff_${xvar}_${yvar}.C
+
+root -b -l -q Veff_${xvar}_${yvar}.C
+
+rm *.root*
+rm *.C*
 
 echo
 echo
